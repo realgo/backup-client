2b7e5f4522629ed0bb81b502f6827f2f046d4e45
Sean Reifschneider <sean@realgo.com>
2013-10-21 11:59:51 -0600

    Initial cut at debian files.

--- debian/backup-client.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the backup-client package
-#
-0 4	* * *	root	[ -x /usr/bin/backup-client_maintenance ] && /usr/bin/backup-client_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(backup-client):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="backup-client" command="/usr/bin/backup-client"
